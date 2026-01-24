cae9fa2b5f6fe010964d30a58ed1c4065ab0a4fa
aralion <aadsf1789@gmail.com>
2026-01-25 05:24:22 +0900

    [Feat] 임시 메인 화면 추가

---
+++ front/src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+// src/pages/MainPage.tsx
+const MainPage = () => {
+  return (
+    <section>
+      <h1>메인 페이지</h1>
+      <p>로그인 후 처음 보이는 화면입니다.</p>
+    </section>
+  );
+};
+
+export default MainPage;
