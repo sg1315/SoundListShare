f1d42c94e7f015bd54b0fe1c9cd7e1fd6541e06c
aralion <aadsf1789@gmail.com>
2026-01-25 05:13:39 +0900

    [Update] 기존 app, main 구조 변경 app -> 여러 데이터 총괄 관리 main -> 라우터 관리 & UI

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// import './App.css';
+import type { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyle from './styles/GlobalStyle';
 
-function App() {
-  const [count, setCount] = useState(0);
+const queryClient = new QueryClient();
 
+function App({ children }: PropsWithChildren) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
