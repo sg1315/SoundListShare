32d7c3efefd07f5d98b2d3383b2cdb1e6ef4decf
aralion <aadsf1789@gmail.com>
2026-01-25 05:24:11 +0900

    [Feat] 기본 레이아웃 추가

--- front/src/components/layout/Layout.tsx
+++ front/src/components/layout/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 const Layout = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <div className="layout">
         {/* <Sidebar /> */}
         <main>
